17658a210f638da3c7955f6538512a5d562fa1bf
JohandroG <johandrogonzalez@gmail.com>
2021-11-26 15:31:09 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+//Imports
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+//Config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Database
+require("./server/config/mongoose.js");
+
+//Routes
+require("./server/routes/tasksRouter.js")(app);
+
+//Port
+app.listen(8080, function(){
+    console.log("Listening on port: 8080");
+})
+
