f8e763e2660c794a585f8597274d1a77b7e4128a
JohandroG <johandrogonzalez@gmail.com>
2021-11-26 15:49:55 -0600

    Changes

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksService } from './tasks/tasks.service';
+//! Importamos el service
+import {HttpClientModule} from '@angular/common/http';
+//! Importamos el httpModule
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TasksService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
